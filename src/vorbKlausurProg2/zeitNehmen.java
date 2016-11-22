f2938bee8f0a5f8ad293aeac90a0527b1590ab80
Lars <lars.kiegeland@t-online.de>
2016-11-22 21:48:31 +0100

    Ein Kommentar entfernt um Push zu testen.

--- src/vorbKlausurProg2/zeitNehmen.java
+++ src/vorbKlausurProg2/zeitNehmen.java
@@ -2,7 +2,6 @@ package vorbKlausurProg2;
 
 public class zeitNehmen {
 	public static void main(String[] args){
-		//Mit diesen Aufrüfen wird die Zeit genommen.
 		long iStart = System.currentTimeMillis();
 		for( long i = 0; i < 5000000L; i++)
 		{
