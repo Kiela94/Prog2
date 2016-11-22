ed4d1d9ff42bf1d5cc5758f353a9bb7ac01826ee
Lars <lars.kiegeland@t-online.de>
2016-11-22 20:48:38 +0100

    Ein Kommentar eingefügt. Testen der Funktionalität mit eclipse

--- src/vorbKlausurProg2/zeitNehmen.java
+++ src/vorbKlausurProg2/zeitNehmen.java
@@ -2,6 +2,7 @@ package vorbKlausurProg2;
 
 public class zeitNehmen {
 	public static void main(String[] args){
+		//Mit diesen Aufrüfen wird die Zeit genommen.
 		long iStart = System.currentTimeMillis();
 		for( long i = 0; i < 5000000L; i++)
 		{
